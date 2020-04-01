2f0b5d05e117a2e7013a27388936ab0555a10b91
andrii <viilka284@gmail.com>
2020-04-01 20:03:32 +0300

    fix landing

--- client/views.py
+++ client/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('landing/index.html')
+
+@app.route('/join')
+def join():
+    return render_template('join/join.html')
