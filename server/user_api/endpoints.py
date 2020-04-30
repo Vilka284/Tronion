1217b08f50ca14ed097359c5cda8a934d0514bdd
andrii <viilka284@gmail.com>
2020-05-01 00:02:04 +0300

    add is_logged function to fix bugs with user views

--- server/user_api/endpoints.py
+++ server/user_api/endpoints.py
@@ -118,3 +118,13 @@ def logout_user():
     response = {'message' : 'ok'}
 
     return response, 200
+
+@user_api.route("/is_logged", methods=["GET"])
+def is_logged():
+
+    data = request.headers["auth_token"]
+    print(data)
+    response = {'message': 'ok'}
+    if data == 0:
+        return response, 400
+    return response, 200
