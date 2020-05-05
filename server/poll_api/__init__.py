0adedefe9c4cebe6fe754b7b0351b605b8db5cca
Rostyslav <43086406+nosoccus@users.noreply.github.com>
2020-05-05 13:19:36 +0300

    create api for polls

---
+++ server/poll_api/__init__.py
@@ -0,0 +1,1 @@
+from .endpoints import poll_api
--- server/poll_api/endpoints.py
+++ server/poll_api/endpoints.py
@@ -1,11 +1,9 @@
 import json
 from flask import Blueprint, current_app, render_template, url_for, session
 from flask_socketio import emit
-# from socketio_examples import socketio
-from flask_socketio import socketio, emit
+from server.app import sio, db
 
-room_api = Blueprint('room_api', __name__, static_folder='static',
-                     template_folder='templates')
+poll_api = Blueprint('poll_api', __name__)
 
 
 class PollTracker(object):
@@ -17,21 +15,25 @@ class PollTracker(object):
 
     def add_vote(self, question, answer):
         """Записуємо новий голос"""
+        print("add_vote")
         self.polls[question][answer] += 1
         self.dirty = True
 
     def remove_vote(self, question, answer):
+        print("remove_vote")
         """Видаляємо голос"""
         self.polls[question][answer] -= 1
         self.dirty = True
 
     def get_tally(self):
+        print("get_tally")
         """Перевірка чи заапдейтились голоса"""
         if self.dirty:
             self.dirty = False
             return self.polls
 
     def set_dirty(self):
+        print("set_dirty")
         """Апдейт флаг"""
         self.dirty = True
 
@@ -39,32 +41,43 @@ class PollTracker(object):
 tally = PollTracker()
 
 
-@room_api.route('/')
-def index():
-    #Return the poll administrator client application.
+@poll_api.route('/')
+def show_results():
+    print("show_results")
+    """Return the poll administrator client application."""
     vote_url = current_app.config.get('POLLS_VOTE_URL') or \
-        url_for('room.vote', _external=True)
-    return render_template('room/results.html', vote_url=vote_url)
+        url_for('poll_api.vote', _external=True)
+    return render_template('polls/results.html', vote_url=vote_url)
 
 
-@socketio.on('connect', namespace='/polls')
+@poll_api.route('/vote')
+def vote():
+    print("vote")
+    """Return the poll participant client application."""
+    return render_template('polls/vote.html')
+
+
+@sio.on('connect', namespace='/polls')
 def on_voter_connect():
+    print("on_voter_connect")
     """Нова сесія учасника голосування."""
     # initialize the votes for this participant
     session['votes'] = {}
 
 
-@socketio.on('disconnect', namespace='/polls')
+@sio.on('disconnect', namespace='/polls')
 def on_voter_disconnect():
     """Учасник відконектився"""
+    print("on_voter_disconnect")
     # remove the votes from this participant
     for question, answer in session['votes'].items():
         tally.remove_vote(question, answer)
 
 
-@socketio.on('vote', namespace='/polls')
+@sio.on('vote', namespace='/polls')
 def on_voter_vote(question, answer):
     """Новий голос"""
+    print("on_voter_vote")
     if question in session['votes']:
         if session['votes'][question] == answer:
             # this is the same vote as before, so it can be ignored
@@ -76,8 +89,9 @@ def on_voter_vote(question, answer):
     tally.add_vote(question, answer)
 
 
-@socketio.on('votes', namespace='/polls')
+@sio.on('votes', namespace='/polls')
 def votes(votes):
+    print("votes")
     """Нові голоса"""
     # remove any previous votes stored in the participant's session
     for question, answer in session['votes'].items():
@@ -88,21 +102,25 @@ def votes(votes):
     session['votes'] = votes
 
 
-@socketio.on('connect', namespace='/polls-admin')
+@sio.on('connect', namespace='/polls-admin')
 def on_admin_connect():
+    print("on_admin_connect")
     """Коннект адміна пулу"""
     # send the current tally
-    emit('update-charts', tally.polls)
+    sio.emit('update-charts', tally.polls)
 
 
 def update_task():
-    """Постійне оновлення графіку при нових голосах"""
+    print("update_task")
+    """Постійне оновлення графіку, коли нові голоса"""
     while True:
         results = tally.get_tally()
+        # results = tally.set_dirty()
+
         if results:
             # broadcast the results to all connected admins
-            socketio.emit('update-charts', results, namespace='/polls-admin')
-        socketio.sleep(5)
+            sio.emit('update-charts', results, namespace='/polls-admin')
+        sio.sleep(5)
 
 
-socketio.start_background_task(update_task)
+sio.start_background_task(update_task)
